37e7d88505eb6b1af073a080345e9927faddb9e1
mo2men-5aled <momenk208@gmail.com>
2022-10-11 18:08:38 +0200

    make the authontecation on a seprate file && add add the auth to the dashboard route it will never show the data untill you log in + use the status code module

--- controllers/main.js
+++ controllers/main.js
@@ -1,25 +1,40 @@
 const CustomAPIError = require("../errors/custom-error");
+const { StatusCodes } = require("http-status-codes");
+
+//import jwt from jsonwebtoken module
 const jwt = require("jsonwebtoken");
 
+//login function
 const login = async (req, res) => {
   const { username, password } = req.body;
 
+  //validate username and password
   if (!username || !password) {
-    throw new CustomAPIError("Please provide a username and password", 400);
+    throw new CustomAPIError(
+      "Please provide a username and password",
+      StatusCodes.BAD_REQUEST
+    );
   }
 
+  //dummy user id
   const id = new Date().getDate();
+
+  //create token
   const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
 
-  res.status(200).json({ msg: "user created", token });
+  res.status(StatusCodes.OK).json({ msg: "user created", token });
 };
 
+//dashboard function
 const dashboard = async (req, res) => {
+  console.log(req);
+
+  //get luck number
   const luckyNumber = Math.floor(Math.random() * 100);
-  res.status(200).json({
-    msg: `Hello, John Doe`,
+  res.status(StatusCodes.OK).json({
+    msg: `Hello, ${req.user.username}`,
     secret: `Here is your authorized data,Your lucky number is ${luckyNumber}`,
   });
 };
---
+++ middleware/auth.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+const { StatusCodes } = require("http-status-codes");
+
+const authenticationMiddleware = async (req, res, next) => {
+  //get token from request header
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError("No token provided", StatusCodes.UNAUTHORIZED);
+  }
+
+  //split token from Bearer
+  const token = authHeader.split(" ")[1];
+
+  try {
+    //verify token by using jwt.verify method that decodes the token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const { id, username } = decoded;
+    req.user = { id, username };
+
+    //call next middleware
+    //if you don't call next middleware, the request will be stuck
+    //and the response will never be sent
+    //so, call next middleware to continue the request
+    //and send the response
+    next();
+  } catch (error) {
+    throw new CustomAPIError(
+      "Not authorized to access this route",
+      StatusCodes.UNAUTHORIZED
+    );
+  }
+};
+
+module.exports = authenticationMiddleware;
